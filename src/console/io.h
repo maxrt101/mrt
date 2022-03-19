7d6042ad5c47c75d1abfb5ebf6532c20e4fccefd
Maksym Tkachuk <s2m4f6@gmail.com>
2022-03-20 01:45:43 +0200

    Implemented readLine & decoding of arrows + ctrl/shift/alt

--- src/console/io.h
+++ src/console/io.h
@@ -1,19 +1,76 @@
 #ifndef _MRT_CONSOLE_IO_H_
 #define _MRT_CONSOLE_IO_H_ 1
 
+#include <string>
+#include <vector>
 #include <cstdio>
 #include <fcntl.h>
+#include <poll.h>
 #include <unistd.h>
 #include <termios.h>
 
 namespace mrt {
 namespace io {
 
-constexpr int ESC = 27;
-constexpr int LEFT_ARROW = 512;
-constexpr int UP_ARROW = 513;
+// Character Flags
+constexpr int CTRL   = 1;
+constexpr int SHIFT  = 2;
+constexpr int ALT    = 4;
+constexpr int OPTION = ALT;
+
+// Character Codes
+constexpr int ESC         = 27;
+constexpr int DEL         = 127;
+constexpr int BELL        = 7;
+constexpr int UP_ARROW    = 512;
+constexpr int DOWN_ARROW  = 513;
 constexpr int RIGHT_ARROW = 514;
-constexpr int DOWN_ARROW = 515;
+constexpr int LEFT_ARROW  = 515;
+
+struct Character {
+  int code;
+  int flags;
+};
+
+Character getChar();
+
+namespace impl {
+
+inline Character decodeArrows(int ch, int flags) {
+  switch (ch) {
+    case 'A': return {UP_ARROW, flags};
+    case 'B': return {DOWN_ARROW, flags};
+    case 'C': return {RIGHT_ARROW, flags};
+    case 'D': return {LEFT_ARROW, flags};
+    default:  return {ch, flags};
+  }
+}
+
+inline Character decodeEscapeSequence(int ch) {
+  switch (ch) {
+    default:  return decodeArrows(ch, 0);
+    case 'Z': return {'\t', SHIFT};
+    case '1': {
+      ch = getchar();
+      if (ch == ';') {
+        ch = getchar();
+        switch (ch) {
+          case '2': return decodeArrows(getchar(), SHIFT);
+          case '3': return decodeArrows(getchar(), ALT);
+          case '4': return decodeArrows(getchar(), SHIFT | ALT);
+          case '5': return decodeArrows(getchar(), CTRL);
+          case '6': return decodeArrows(getchar(), CTRL | SHIFT);
+          case '7': return decodeArrows(getchar(), CTRL | ALT);
+          case '8': return decodeArrows(getchar(), CTRL | SHIFT | ALT);
+          default:  return {ch}; // TODO: Find list of combinations
+        }
+      }
+    }
+  }
+  return {ch};
+}
+
+} /* namespace impl */
 
 inline termios getTerminalSettings() {
   termios settings;
@@ -47,7 +104,7 @@ inline void setEcho(bool flag) {
   tcsetattr(STDIN_FILENO, TCSANOW, &settings);
 }
 
-void showCursor(bool flag) {
+inline void showCursor(bool flag) {
   if (flag) {
     fputs("\e[?25h", stdout);
   } else {
@@ -55,23 +112,49 @@ void showCursor(bool flag) {
   }
 }
 
-inline int getChar() {
+inline void clearLine() {
+  printf("\e[2K");
+}
+
+inline void clearScreen() {
+  printf("\e[2K");
+}
+
+inline void moveCursorInLine(int offset) {
+  printf("\e[%dG", offset);
+}
+
+inline void moveCursor(int rows, int cols) {
+  printf("\e[%d;%dH", rows, cols);
+}
+
+inline void moveCursorUp(int n = 1) {
+  printf("\e[%dA", n);
+}
+
+inline void moveCursorDown(int n = 1) {
+  printf("\e[%dB", n);
+}
+
+inline void moveCursorRight(int n = 1) {
+  printf("\e[%dC", n);
+}
+
+inline void moveCursorLeft(int n = 1) {
+  printf("\e[%dD", n);
+}
+
+inline Character getChar() {
   int ch = 0;
   ch = getchar();
   if (ch == ESC) {
     ch = getchar();
     if (ch == '[') {
       ch = getchar();
-      switch (ch) {
-        case 'A': return UP_ARROW;
-        case 'B': return DOWN_ARROW;
-        case 'C': return RIGHT_ARROW;
-        case 'D': return LEFT_ARROW;
-        default:  return ch;
-      }
+      return impl::decodeEscapeSequence(ch);
     }
   }
-  return ch;
+  return {ch};
 }
 
 } /* namespace io */
---
+++ src/console/readline.h
@@ -0,0 +1,96 @@
+#ifndef _MRT_CONSOLE_READLINE_H_
+#define _MRT_CONSOLE_READLINE_H_ 1
+
+#include <mrt/console/io.h>
+
+namespace mrt {
+namespace io {
+
+inline std::string readLine(const std::string& prompt, std::vector<std::string>& history) {
+  auto termSettings = getTerminalSettings();
+  setOneCharRead(true);
+  setEcho(false);
+
+  history.push_back("");
+
+  int hIndex = history.size() - 1; // history index - index into history
+  int sIndex = 0; // string index - index into curent line
+
+  bool read = true;
+
+  printf("%s", prompt.c_str());
+  while (read) {
+    Character ch = getChar();
+    switch (ch.code) {
+      case UP_ARROW: {
+        if (hIndex >= 0) {
+          printf("\r%s%s", prompt.c_str(), history[--hIndex].c_str());
+        } else {
+          putchar(BELL);
+        }
+        break;
+      }
+      case DOWN_ARROW: {
+        if (hIndex < history.size()) {
+          printf("\r%s%s", prompt.c_str(), history[--hIndex].c_str());
+        } else {
+          putchar(BELL);
+        }
+        break;
+      }
+      case LEFT_ARROW: {
+        // if (ch.flags & CTRL) {}
+        if (sIndex > 0) {
+          sIndex--;
+          moveCursorLeft();
+        } else {
+          putchar(BELL);
+        }
+        break;
+      }
+      case RIGHT_ARROW: {
+        // if (ch.flags & CTRL) {}
+        if (sIndex < history[hIndex].size()) {
+          sIndex++;
+          moveCursorRight();
+        } else {
+          putchar(BELL);
+        }
+        break;
+      }
+      case '\b':
+      case DEL: {
+        if (sIndex == 0) {
+          putchar(BELL);
+        } else {
+          clearLine();
+          history[hIndex].erase(history[hIndex].begin() + --sIndex);
+          printf("\r%s%s", prompt.c_str(), history[hIndex].c_str());
+          moveCursorInLine(prompt.size() + sIndex + 1); // Put cursor to sIndex
+        }
+        break;
+      }
+      case '\n': {
+        putchar('\n');
+        goto end;
+      }
+      default: {
+        history[hIndex].insert(history[hIndex].begin() + sIndex++, ch.code);
+        printf("\r%s%s", prompt.c_str(), history[hIndex].c_str());
+      }
+    }
+  }
+
+end:
+  setEcho(true);
+  setTerminalSettings(termSettings);
+  if (hIndex != history.size() - 1) {
+    history.push_back(history[hIndex]);
+  }
+  return history.back();
+}
+
+} /* namespace io */
+} /* namespace mrt */
+
+#endif /* _MRT_CONSOLE_READLINE_H_ */
