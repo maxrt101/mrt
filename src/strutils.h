8474511d1d5be962f6eef2ef48964e2d0945a199
Maksym Tkachuk <s2m4f6@gmail.com>
2022-03-20 04:53:49 +0200

    Added str::splitQuoted

--- src/strutils.h
+++ src/strutils.h
@@ -29,6 +29,31 @@ inline std::vector<std::string> split(const std::string& str, const std::string&
   return result;
 }
 
+inline std::vector<std::string> splitQuoted(const std::string& str, const std::string& delimiter = " ") {
+  std::vector<std::string> result;
+  size_t last = 0, next = 0;
+  while ((next = str.find(delimiter, last)) != std::string::npos) {
+    result.push_back(str.substr(last, next-last));
+    last = next + 1;
+  }
+  result.push_back(str.substr(last));
+  for (int i = 0; i < result.size(); i++) {
+    if (result[i][0] == '\'' || result[i][0] == '\"') {
+      char quote = result[i][0];
+      std::string token = result[i];
+      int j = i++;
+      while (i < result.size() && result[i][result[i].size()-1] != quote) {
+        token += " " + result[i++];
+      }
+      token += " " + result[i++];
+      result.erase(result.begin()+j, result.begin()+i);
+      i = j;
+      result.insert(result.begin() + i, token.substr(1, token.size()-2));
+    }
+  }
+  return result;
+}
+
 } // namespace str
 } // namespace mrt
 
